3ace6a2e68a7e5acf52e716eb224de264b5cd26f
Polet <poletjimenezdemiguel@hotmail.com>
2024-03-07 06:56:55 -0300

    add parcel for building.

---
+++ new.js
@@ -0,0 +1,15 @@
+var mont = document.getElementById('mont');
+var display = 0;
+function hideShoww()
+{
+    if(display == 1)
+    {
+      div.style.display = 'block';
+      display = 0;    
+    }
+    else
+    {
+      div.style.display = 'none';
+      display = 1;    
+    }
+}
